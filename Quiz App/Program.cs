e8d50a00ee843ef00cc666513d6404c5ef7fce3e
HiCapa9 <hicapa91@gmail.com>
2024-12-09 19:13:14 +0200

    Added Quiz class and fuctions to said class

--- Quiz App/Program.cs
+++ Quiz App/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Question[] questions = new Question[1] 
+            { 
+                new Question("What is the capital of France? " , 
+                new string[] {"Paris", "London", "Berlin", "Madrid" },
+                1
+                )
+
+            };
+
+            Quiz quiz = new Quiz(questions);
+            quiz.DisplayQuestion(questions[0]);
+
+            Console.ReadLine();
+
         }
     }
 }
---
+++ Quiz App/Quiz.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz_App
+{
+    internal class Quiz
+    {
+        private Question[] questions;
+
+        public Quiz(Question[] questions)
+        {
+            this.questions = questions;
+        }
+
+        public void DisplayQuestion(Question question) {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(question.QuestionText);
+            Console.ResetColor();
+
+            for (int i = 0; i < question.Answers.Length; i++) {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.Write("    ");
+                Console.Write(i + 1);
+                Console.ResetColor();
+                Console.WriteLine($". {question.Answers[i]}");
+            }
+
+            if (GetUserChoice() == question.CorrectAnswerIndex)
+            {
+                Console.WriteLine("Correct!");
+            }
+            else {
+                Console.WriteLine("Incorrect");
+            }
+
+        }
+
+        private int GetUserChoice() {
+            Console.WriteLine("Your answer (number): ");
+            string input = Console.ReadLine();
+            int choice = 0;
+            while (!int.TryParse(input, out choice) || choice < 1 || choice > 4)
+            {
+                Console.WriteLine("Invalid choice. Please enter a numvber between 1 and 4: ");
+                input = Console.ReadLine();
+            }
+            return choice - 1;
+
+        }
+
+
+    }
+}
